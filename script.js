ef8dd96236b6abebf78c584e57b602940934e1e4
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 function minDate(dates) {
-  //write you code here
+  if (!dates || dates.length === 0) {
+    return null;
+  }
+
+  // Clone the array to avoid modifying the input array
+  const clonedDates = [...dates];
+
+  // Parse the first date as the initial minimum
+  let minDateValue = new Date(clonedDates[0]);
+
+  // Loop through the remaining dates and update the minimum if needed
+  for (let i = 1; i < clonedDates.length; i++) {
+    const currentDate = new Date(clonedDates[i]);
+
+    if (currentDate < minDateValue) {
+      minDateValue = currentDate;
+    }
+  }
+
+  // Format the minimum date as "YYYY/MM/DD"
+  const minDateFormatted = `${minDateValue.getFullYear()}/${padZeroes(
+    minDateValue.getMonth() + 1
+  )}/${padZeroes(minDateValue.getDate())}`;
+
+  return minDateFormatted;
 }
 
-// Do not change the code
+// Helper function to pad zeroes for single-digit months and days
+function padZeroes(number) {
+  return number.toString().padStart(2, '0');
+}
 
-var dates = [
-  "2023/03/01",
-  "2023/03/02",
-  "2023/03/03",
-  "2023/03/04",
-  "2023/03/05",
-  "2023/03/06",
-  "2023/03/07",
-  "2023/03/08",
-  "2023/03/09",
-  "2023/03/10",
-  "2023/03/11",
-  "2023/03/12",
-  "2023/03/13",
-  "2023/03/14",
-  "2023/03/15",
-  "2023/03/16",
-  "2023/03/17",
-  "2023/03/18",
-  "2023/03/19",
-  "2023/03/20",
-  "2023/03/21",
-  "2023/03/22",
-  "2023/03/23",
-  "2023/03/24",
-  "2023/03/25",
-  "2023/03/26",
-  "2023/03/27",
-  "2023/03/28",
-  "2023/03/29",
-  "2023/03/30",
-];
+// Examples
+console.log(minDate(["2023/03/01", "2023/03/02", "2023/03/03"])); // "2023/03/01"
+console.log(minDate(["2023/01/01", "2023/02/02", "2022/12/31"])); // "2022/12/31"
 
-alert(minDate(dates));
